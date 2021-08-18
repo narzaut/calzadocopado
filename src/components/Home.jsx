ff425435d9f417691804881dbe5f42d560c53db8
narz <i.arzaut@itecriocuarto.org.ar>
2021-08-18 00:27:38 -0300

    first deploy

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { About } from './About'
-
+import Carousel from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 import { Products } from './Products'
 import { Footer } from './Footer'
 const TopBar = () => {
@@ -45,6 +46,7 @@ const Header = () => {
 }
 
 export const Home = () => {
+	const [width, setWidth] = useState(window.innerWidth);
   	
 	return (
     <div  className='text-shadow  w-screen  overflow-x-hidden text-white'>
@@ -52,6 +54,29 @@ export const Home = () => {
 			
 			<Products />
 			<About />
+			<div className='flex h-full w-full items-start justify-center '>
+				<div className='flex flex-col w-full px-10 lg:px-10 lg:w-3/4 h-full py-20 items-center justify-center'>
+					<div className='pb-6'>
+						<p className='border-b-4 text-lg lg:text-2xl border-blue-400 max-w-max text-gray-700 uppercase'>COMBOS</p>
+					</div>
+					{width > 600 ? 
+						<Carousel plugins={['arrows']}>
+    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/254621/inviernosale-slider-13-08-21-dsk.gif?v=637644937729000000'} />
+    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/248337/especialnaranja-260521-slider-desk-v2.gif?v=637590281630800000'} />
+    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/254621/inviernosale-slider-13-08-21-dsk.gif?v=637644937729000000'} />
+
+  					</Carousel>
+					:
+						<Carousel>
+    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/254621/inviernosale-slider-13-08-21-dsk.gif?v=637644937729000000'} />
+    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/248337/especialnaranja-260521-slider-desk-v2.gif?v=637590281630800000'} />
+    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/254621/inviernosale-slider-13-08-21-dsk.gif?v=637644937729000000'} />
+  					</Carousel>
+					}
+				</div>
+				
+			</div>
+			
 			<Footer />
 		</div>
   );
