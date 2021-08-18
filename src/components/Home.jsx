24e7928cf43cb151b687a2abf7d6c0e91bbed0ce
narz <i.arzaut@itecriocuarto.org.ar>
2021-08-18 00:35:06 -0300

    add href

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useState} from 'react';
 import { About } from './About'
-import Carousel from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
+
 import { Products } from './Products'
 import { Footer } from './Footer'
+import { Combos } from './Combos'
+
 const TopBar = () => {
 	return (
 		<div  className='bg-transparent w-full h-48 lg:h-20 flex flex-col md:flex-row lg:flex-row '>
@@ -15,7 +16,7 @@ const TopBar = () => {
 					<a href='#products' className='smoothscroll products pl-4 hover:text-blue-400 transition'>
 						<h1>Productos</h1>
 					</a>
-					<a href='' className='combos pl-4 lg:pl-10 hover:text-blue-400 transition'>
+					<a href='#combos' className='combos pl-4 lg:pl-10 hover:text-blue-400 transition'>
 						<h1>Combos</h1>
 					</a>
 					<a href='#about' className='about px-4 lg:px-10 hover:text-blue-400 transition'>
@@ -46,7 +47,6 @@ const Header = () => {
 }
 
 export const Home = () => {
-	const [width, setWidth] = useState(window.innerWidth);
   	
 	return (
     <div  className='text-shadow  w-screen  overflow-x-hidden text-white'>
@@ -54,28 +54,7 @@ export const Home = () => {
 			
 			<Products />
 			<About />
-			<div className='flex h-full w-full items-start justify-center '>
-				<div className='flex flex-col w-full px-10 lg:px-10 lg:w-3/4 h-full py-20 items-center justify-center'>
-					<div className='pb-6'>
-						<p className='border-b-4 text-lg lg:text-2xl border-blue-400 max-w-max text-gray-700 uppercase'>COMBOS</p>
-					</div>
-					{width > 600 ? 
-						<Carousel plugins={['arrows']}>
-    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/254621/inviernosale-slider-13-08-21-dsk.gif?v=637644937729000000'} />
-    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/248337/especialnaranja-260521-slider-desk-v2.gif?v=637590281630800000'} />
-    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/254621/inviernosale-slider-13-08-21-dsk.gif?v=637644937729000000'} />
-
-  					</Carousel>
-					:
-						<Carousel>
-    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/254621/inviernosale-slider-13-08-21-dsk.gif?v=637644937729000000'} />
-    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/248337/especialnaranja-260521-slider-desk-v2.gif?v=637590281630800000'} />
-    					<img src={'https://topperarg.vteximg.com.br/arquivos/ids/254621/inviernosale-slider-13-08-21-dsk.gif?v=637644937729000000'} />
-  					</Carousel>
-					}
-				</div>
-				
-			</div>
+			<Combos />
 			
 			<Footer />
 		</div>
